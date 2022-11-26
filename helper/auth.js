0d8999a7f55f6be5f19f8c06577f2cc1d52c4ed5
Hai Bui <haibui.cs@gmail.com>
2022-11-26 15:15:43 +0700

    sketch

--- helper/auth.js
+++ helper/auth.js
@@ -1,6 +1,6 @@
 module.exports = {
   auth: (req, res, next) => {
-    if (!req.session.userid) {
+    if (!req.session.userId) {
       return res.status(403).send("No permission");
     }
     next();
